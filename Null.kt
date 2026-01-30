6eea118401dac9b636abb4c547be859c0e22aa8a
Leonardo Alexandro Rodríguez Ortiz <160083135+lioneldkr4@users.noreply.github.com>
2026-01-30 17:37:52 -0600

    Create Null.kt

---
+++ Null.kt
@@ -0,0 +1,92 @@
+//Vemos sobre casos de uso comunes para funciones de seguridad nulas
+
+//Ejercicio 1
+data class User(val name: String?)
+
+fun getNotificationPreferences(user: Any, emailEnabled: Boolean, smsEnabled: Boolean): List<String> {
+    val validUser = user as? User ?: return emptyList()
+    val userName = validUser.name ?: "Guest"
+
+    return listOfNotNull(
+        "Email Notifications enabled for $userName".takeIf { emailEnabled },
+        "SMS Notifications enabled for $userName".takeIf { smsEnabled }
+    )
+}
+
+fun main() {
+    val user1 = User("Alice")
+    val user2 = User(null)
+    val invalidUser = "NotAUser"
+
+    println(getNotificationPreferences(user1, emailEnabled = true, smsEnabled = false))
+    // [Email Notifications enabled for Alice]
+    println(getNotificationPreferences(user2, emailEnabled = false, smsEnabled = true))
+    // [SMS Notifications enabled for Guest]
+    println(getNotificationPreferences(invalidUser, emailEnabled = true, smsEnabled = true))
+    // []
+}
+
+//Ejercicio 2
+data class Subscription(val name: String, val isActive: Boolean)
+
+fun getActiveSubscription(subscriptions: List<Subscription>): Subscription? {
+    return subscriptions.singleOrNull { subscription -> subscription.isActive }
+}
+
+fun main() {
+    val userWithPremiumPlan = listOf(
+        Subscription("Basic Plan", false),
+        Subscription("Premium Plan", true)
+    )
+
+    val userWithConflictingPlans = listOf(
+        Subscription("Basic Plan", true),
+        Subscription("Premium Plan", true)
+    )
+
+    println(getActiveSubscription(userWithPremiumPlan))
+    // Subscription(name=Premium Plan, isActive=true)
+
+    println(getActiveSubscription(userWithConflictingPlans))
+    // null
+}
+
+//Ejecicio 3
+data class User(val username: String, val isActive: Boolean)
+
+fun getActiveUsernames(users: List<User>): List<String> {
+    return users.mapNotNull { user ->
+        if (user.isActive) user.username else null
+    }
+}
+
+fun main() {
+    val allUsers = listOf(
+        User("alice123", true),
+        User("bob_the_builder", false),
+        User("charlie99", true)
+    )
+
+    println(getActiveUsernames(allUsers))
+    // [alice123, charlie99]
+}
+
+//Ejercicio 4
+fun validateStock(requested: Int?, available: Int?): Int {
+    val validRequested = requested ?: return -1
+    val validAvailable = available ?: return -1
+
+    if (validRequested < 0) return -1
+    if (validRequested > validAvailable) return -1
+
+    return validRequested
+}
+
+fun main() {
+    println(validateStock(5,10))
+    // 5
+    println(validateStock(null,10))
+    // -1
+    println(validateStock(-2,10))
+    // -1
+}
