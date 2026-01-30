89af4e631a87f2a23ebccd553bffdc9acff59297
Leonardo Alexandro Rodríguez Ortiz <160083135+lioneldkr4@users.noreply.github.com>
2026-01-30 17:16:09 -0600

    Create Scope.kt

---
+++ Scope.kt
@@ -0,0 +1,45 @@
+//Usamos funciones de alcance para escribir código
+
+//Ejercicio 1
+data class ProductInfo(val priceInDollars: Double?)
+
+class Product {
+    fun getProductInfo(): ProductInfo? {
+        return ProductInfo(100.0)
+    }
+}
+
+fun Product.getPriceInEuros() = getProductInfo()?.priceInDollars?.let { convertToEuros(it) }
+
+fun convertToEuros(dollars: Double): Double {
+    return dollars * 0.85
+}
+
+fun main() {
+    val product = Product()
+    val priceInEuros = product.getPriceInEuros()
+
+    if (priceInEuros != null) {
+        println("Price in Euros: €$priceInEuros")
+        // Price in Euros: €85.0
+    } else {
+        println("Price information is not available.")
+    }
+}
+
+//Ejercicio 2
+data class User(val id: Int, var email: String)
+
+fun updateEmail(user: User, newEmail: String): User = user.apply {
+    this.email = newEmail
+}.also { println("Updating email for user with ID: ${it.id}") }
+
+fun main() {
+    val user = User(1, "old_email@example.com")
+    val updatedUser = updateEmail(user, "new_email@example.com")
+    // Updating email for user with ID: 1
+
+    println("Updated User: $updatedUser")
+    // Updated User: User(id=1, email=new_email@example.com)
+}
+
