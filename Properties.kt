56b60c19d27da928eb13eea619ab129cededc6b5
Leonardo Alexandro Rodríguez Ortiz <160083135+lioneldkr4@users.noreply.github.com>
2026-01-30 17:34:20 -0600

    Create Properties.kt

---
+++ Properties.kt
@@ -0,0 +1,80 @@
+//Aprendemos mas de como se usan las propiedades en kotlin
+
+//Ejercicio 1
+fun findOutOfStockBooks(inventory: List<Int>): List<Int> {
+    val outOfStockIndices = mutableListOf<Int>()
+    for (index in inventory.indices) {
+        if (inventory[index] == 0) {
+            outOfStockIndices.add(index)
+        }
+    }
+    return outOfStockIndices
+}
+
+fun main() {
+    val inventory = listOf(3, 0, 7, 0, 5)
+    println(findOutOfStockBooks(inventory))
+    // [1, 3]
+}
+
+//Ejercicio 2
+val Double.asMiles: Double
+    get() = this * 0.621371
+
+fun main() {
+    val distanceKm = 5.0
+    println("$distanceKm km is ${distanceKm.asMiles} miles")
+    // 5.0 km is 3.106855 miles
+
+    val marathonDistance = 42.195
+    println("$marathonDistance km is ${marathonDistance.asMiles} miles")
+    // 42.195 km is 26.218757 miles
+}
+
+//Ejercicio 3
+fun checkAppServer(): Boolean {
+    println("Performing application server health check...")
+    return true
+}
+
+fun checkDatabase(): Boolean {
+    println("Performing database health check...")
+    return false
+}
+
+fun main() {
+    val isAppServerHealthy by lazy { checkAppServer() }
+    val isDatabaseHealthy by lazy { checkDatabase() }
+
+    when {
+        isAppServerHealthy -> println("Application server is online and healthy")
+        isDatabaseHealthy -> println("Database is healthy")
+        else -> println("System is offline")
+    }
+   // Performing application server health check...
+   // Application server is online and healthy
+}
+
+//Ejercicio 4
+import kotlin.properties.Delegates.observable
+
+class Budget(val totalBudget: Int) {
+    var remainingBudget: Int by observable(totalBudget) { _, oldValue, newValue ->
+        if (newValue < totalBudget * 0.2) {
+            println("Warning: Your remaining budget ($newValue) is below 20% of your total budget.")
+        } else if (newValue > oldValue) {
+            println("Good news: Your remaining budget increased to $newValue.")
+        }
+    }
+}
+
+fun main() {
+    val myBudget = Budget(totalBudget = 1000)
+    myBudget.remainingBudget = 800
+    myBudget.remainingBudget = 150
+    // Warning: Your remaining budget (150) is below 20% of your total budget.
+    myBudget.remainingBudget = 50
+    // Warning: Your remaining budget (50) is below 20% of your total budget.
+    myBudget.remainingBudget = 300
+    // Good news: Your remaining budget increased to 300.
+}
